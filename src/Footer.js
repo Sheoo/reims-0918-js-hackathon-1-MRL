ecad7c7bb8b21bdb802fefc7f3c9f0c305f34b45
LeuthsoulineChanthathirath <r.leuthsouline@gmail.com>
2018-10-30 23:02:07 +0100

    HeaderFooter

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+import { Container, Row, Col } from "reactstrap";
+
+const Footer = () => (
+  <Container fluid>
+    <Row>
+      <Col className="text" sm="6">
+        Made with <span className="heart">❤</span> by Robin - Michael -{" "}
+        Leuthsouline
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+  <header style={{ textAlign: "center" }}>
+    <img src="images/Logo-blanc.png" className="center" alt="logo candy" />
+    <p className="text-center">Come hunt your halloween sweets</p>
+  </header>
+);
+
+export default Header;
